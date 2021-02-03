21d9e94c295392dd63d509a20affcc3a3f1dbf15
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2021-02-03 19:59:14 +0600

    Working with model

---
+++ model/model.php
@@ -0,0 +1,15 @@
+<?php
+    include "./book.php";
+    class Model{
+        public function getBookDetails(){
+         array(
+             "xyz"=> new Book('xyz Book','Kamran Hafiz','History Book'),
+             "Jungle Book"=> new Book('Jungle Book','R.Kipling','Classic Book')
+        );
+        }
+        public function getBook(){
+            $allBooks = $this->getBookDetails();
+            return $allBooks['$title']
+        }
+    }
+?>
